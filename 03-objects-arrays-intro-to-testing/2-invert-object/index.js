61e0bf0e3b8cfc16ce28d09d70299e48ece772e9
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-08-31 14:30:08 +0300

    invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,12 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (!obj) {
+    return
+  }
+  return Object.keys(obj).length
+    ? Object.entries(obj)
+        .map(([key, val]) => ({ [val]: key }))
+        .reduce((acc, cur) => ({ ...acc, ...cur }), {})
+    : {}
 }
