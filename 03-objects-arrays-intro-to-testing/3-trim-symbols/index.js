30bc5b171075895fb0a9ca471d84a493616bb7d8
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2020-09-01 11:46:11 +0300

    trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  const chunks = string.split('').reduce(
+    (acc, curLetter, ind, arr) => {
+      const isSameLetter = acc.lastLetter === curLetter
+      if (isSameLetter) {
+        acc.lastChunk = [...acc.lastChunk, curLetter]
+      } else {
+        acc.lastLetter = curLetter
+        acc.chunks = [...acc.chunks, acc.lastChunk]
+        acc.lastChunk = [curLetter]
+      }
+      return ind < arr.length - 1 ? acc : [...acc.chunks, acc.lastChunk]
+    },
+    { chunks: [], lastChunk: [], lastLetter: string[0] },
+  )
+  return string === ''
+    ? ''
+    : chunks.map((chunk) => chunk.join('').substring(0, size)).join('')
 }
